fb8c1e732fded8d2bd785e61e3cd84c450c440fc
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-04-06 18:07:24 +0100

    Fixed checksum in tx. Make sure is 8 bit length

--- raspberry_tx/tests/IR/c/ir-slinger/i2c_example.py
+++ raspberry_tx/tests/IR/c/ir-slinger/i2c_example.py
@@ -8,10 +8,15 @@ msg_bytes = [0,0,0,0,0]
 f = open("expected_message.txt", "rb")
 expected_message = int(f.read())
 
+import pdb; pdb.set_trace()
 for i in range(5):
     msg_bytes[i] = expected_message >> (8*i) & 0xff
-    if i == 4: 
-        msg_bytes[4] = sum(bytearray(msg_bytes))
+    if i == 4:
+        for j in range(4):
+            msg_bytes[4] += msg_bytes[j]
+        msg_bytes[4] = msg_bytes[4] & 0xff
+    import pdb; pdb.set_trace()
+
     bus.write_byte(addr, msg_bytes[i])
     time.sleep(0.07)
 
