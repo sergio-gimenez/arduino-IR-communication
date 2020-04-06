f822ea5e4f2604da81e9e5c1ab94fa2844c0aa75
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-04-06 16:16:43 +0100

    Added checksum in tx

--- raspberry_tx/tests/IR/c/ir-slinger/i2c_example.py
+++ raspberry_tx/tests/IR/c/ir-slinger/i2c_example.py
@@ -3,17 +3,19 @@ import time
 
 addr = 0x8 # bus address
 bus = SMBus(1) # indicates /dev/ic2-1
-msg_bytes = [0,0,0,0] 
+msg_bytes = [0,0,0,0,0] 
 
 f = open("expected_message.txt", "rb")
 expected_message = int(f.read())
 
-for i in range(4):
+for i in range(5):
     msg_bytes[i] = expected_message >> (8*i) & 0xff
+    if i == 4: 
+        msg_bytes[4] = sum(bytearray(msg_bytes))
     bus.write_byte(addr, msg_bytes[i])
-    time.sleep(0.02)
+    time.sleep(0.07)
+
 
 #bus.write_i2c_block_data(addr, 0, msg_bytes)
-
 print ("Message sent: "+ hex(expected_message))
 
