23da6be632919e84914239a9418ceebacf78d2ec
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-04-15 16:29:47 +0200

    Added code for getting the first readings from photodiode

---
+++ arduino_rx/photodiode_test/photodiode_test.ino
@@ -0,0 +1,18 @@
+int pd = 2;                    //Photodiode to digital pin 2
+int buzz = 13;                 //piezo buzzer to digital pin 13
+int senRead = 0;               //Readings from sensor to analog pin 0
+int limit = 850;               //Threshold range of an obstacle
+void setup()
+{
+  //  pinMode(pd, OUTPUT);
+  //  pinMode(buzz, OUTPUT);
+  //  digitalWrite(pd, HIGH);      //supply 5 volts to photodiode
+  //  digitalWrite(buzz, LOW);     //set the buzzer in off mode (initial condition)
+  Serial.begin(9600);          //setting serial monitor at a default baund rate of 9600
+}
+void loop()
+{
+  int val = analogRead(senRead); //variable to store values from the photodiode
+  Serial.println(val);          // prints the values from the sensor in serial monitor
+  delay(100);
+}
