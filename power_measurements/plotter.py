dca6559a73e48642f7d05051f41abceb0d59a18a
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-04-23 13:39:57 +0200

    Added python code for print pream rx voltage

--- power_measurements/plotter.py
+++ power_measurements/plotter.py
@@ -4,20 +4,27 @@ import matplotlib.pyplot as plt
 import csv
 
 time = []
-voltage = []
+v_rx_amp = []
+v_rx_preamp = []
 
 # Read the file and save it in an array without the \n character
 # Each position has: [V,t]
 out = open("out.csv", "r").read().splitlines()
 for i in out:
-    voltage.append(float(i.split(",")[0]))
-    time.append(int(i.split(",")[1]))
+    v_rx_amp.append(float(i.split(",")[0]))
+    v_rx_preamp.append(float(i.split(",")[1]))
+    time.append(int(i.split(",")[2]))
 
-plt.plot(time,voltage)
+
+# Plot formatting
+plt.plot(time,v_rx_amp, label = "Amplified Rx V")
+plt.plot(time,v_rx_preamp, label = "Pre-amplified Rx V")
+
 plt.xlabel('Time [s]')
 plt.ylabel('Voltage [mV]')
-plt.title('Received Voltage\nReceived 6 messages (32bit each)')
-plt.xlim(0,3000)
+plt.title('IR Transmission\nReceived 6 messages (32bit each)')
+plt.xlim(0,3500)
 plt.grid()
+plt.legend()
 plt.show()
 
--- power_measurements/voltage_measurements/voltage_measurements.ino
+++ power_measurements/voltage_measurements/voltage_measurements.ino
@@ -1,20 +1,16 @@
 int has_tx_started = false;
 unsigned int start_time;
-int expected_messages = 5;
-int i = 0;
 boolean isEOT = false;
 
-
-int rx_amp_pin = 0;
-int rx_amp;
-float v_rx_amp;
+int rx_amp_pin = 0; // Analog pin for read
+int rx_amp;         // Quantized measured value
+float v_rx_amp;     // Voltage measured in mV
 
 int rx_preamp_pin = 2;
 int rx_preamp;
 float v_rx_preamp;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
 
   // In case the interrupt driver crashes on setup, give a clue
@@ -22,38 +18,40 @@ void setup()
   //This message can be removed if recording data into file
 
   //Serial.println("Waiting for an IR packet");
-
 }
 
-void loop()
-{
-
+void loop() {
   rx_amp = analogRead(rx_amp_pin);
   rx_preamp = analogRead(rx_preamp_pin);
 
-
+  // Start program if message received and while transmission is not over
   if ((rx_amp > 300) && !isEOT) {
     has_tx_started = true;
     start_time = millis();
 
     while (has_tx_started && !isEOT) {
+
+      // TODO program function
+      // Convert quantized value measured from arduino to mV
       v_rx_amp = (float(rx_amp) * 5 / 1024) * 1000;
       v_rx_preamp = (float(rx_preamp) * 5 / 1024) * 1000;
 
+      // TODO program function
       // Print stuff in pseudo-csv format
       Serial.print(v_rx_amp);
       Serial.print(",");
       Serial.print(v_rx_preamp);
-      Serial.print(",");      
+      Serial.print(",");
       Serial.println(millis() - start_time);
+
       rx_amp = analogRead(rx_amp_pin);
       rx_preamp = analogRead(rx_preamp_pin);
-      
+
       // Expected 6 IR messages from raspi
       // That's enough time to the messages to arrive. If they don't, something happened
       if ((millis() - start_time) > 4500) {
         isEOT = true;
-        
+
         //This message can be removed if recording data into file
         //Serial.println("EOT");
       }
