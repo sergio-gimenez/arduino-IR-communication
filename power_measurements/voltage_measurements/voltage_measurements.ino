48a5b7667179506cb91149cae589bc53421301c0
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-04-23 13:15:05 +0200

    Added arduino code for reading the preamp rx voltage

--- power_measurements/voltage_measurements/voltage_measurements.ino
+++ power_measurements/voltage_measurements/voltage_measurements.ino
@@ -1,19 +1,26 @@
-
-int senRead = 0;               //Readings from sensor to analog pin 0
 int has_tx_started = false;
 unsigned int start_time;
 int expected_messages = 5;
 int i = 0;
 boolean isEOT = false;
-float quantized_voltage;
+
+
+int rx_amp_pin = 0;
+int rx_amp;
+float v_rx_amp;
+
+int rx_preamp_pin = 2;
+int rx_preamp;
+float v_rx_preamp;
 
 void setup()
 {
-  Serial.begin(9600);    //setting serial monitor at a default baund rate of 9600
+  Serial.begin(9600);
 
   // In case the interrupt driver crashes on setup, give a clue
   // to the user what's going on.
   //This message can be removed if recording data into file
+
   //Serial.println("Waiting for an IR packet");
 
 }
@@ -21,27 +28,34 @@ void setup()
 void loop()
 {
 
-  int val = analogRead(senRead); //variable to store values from the photodiode
+  rx_amp = analogRead(rx_amp_pin);
+  rx_preamp = analogRead(rx_preamp_pin);
 
-  if ((val > 300) && !isEOT) {
+
+  if ((rx_amp > 300) && !isEOT) {
     has_tx_started = true;
     start_time = millis();
 
     while (has_tx_started && !isEOT) {
-      if (i < 5) {
-        quantized_voltage = (float(val) * 5 / 1024) * 1000;
-        Serial.print(quantized_voltage);          // prints the values from the sensor in serial monitor
-        Serial.print(",");
-        Serial.println(millis() - start_time);
-        val = analogRead(senRead); //variable to store values from the photodiode
+      v_rx_amp = (float(rx_amp) * 5 / 1024) * 1000;
+      v_rx_preamp = (float(rx_preamp) * 5 / 1024) * 1000;
 
-        // Expected 6 IR messages from raspi
-        // That's enough time to the messages to arrive. If they don't, something happened
-        if ((millis() - start_time) > 4500) {
-          isEOT = true;
-          //This message can be removed if recording data into file
-          //Serial.println("EOT");
-        }
+      // Print stuff in pseudo-csv format
+      Serial.print(v_rx_amp);
+      Serial.print(",");
+      Serial.print(v_rx_preamp);
+      Serial.print(",");      
+      Serial.println(millis() - start_time);
+      rx_amp = analogRead(rx_amp_pin);
+      rx_preamp = analogRead(rx_preamp_pin);
+      
+      // Expected 6 IR messages from raspi
+      // That's enough time to the messages to arrive. If they don't, something happened
+      if ((millis() - start_time) > 4500) {
+        isEOT = true;
+        
+        //This message can be removed if recording data into file
+        //Serial.println("EOT");
       }
     }
   }
