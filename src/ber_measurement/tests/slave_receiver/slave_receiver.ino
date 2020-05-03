5af50d3c886fa19f2f98acad1b6948d9921701bc
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-05-03 16:29:17 +0200

    Changed serial port baudrate

--- src/ber_measurement/tests/slave_receiver/slave_receiver.ino
+++ src/ber_measurement/tests/slave_receiver/slave_receiver.ino
@@ -29,7 +29,7 @@ int offset;
 void setup() {
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
+  Serial.begin(38400);           // start serial for output
   Serial.println("Ready to receive");
 }
 
