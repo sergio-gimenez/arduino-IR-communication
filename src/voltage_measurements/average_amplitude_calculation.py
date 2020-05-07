21a23efe48983938c6fa350d2e7f30de13c04a3d
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-05-07 17:21:04 +0200

    Added colors to the plot

--- src/voltage_measurements/average_amplitude_calculation.py
+++ src/voltage_measurements/average_amplitude_calculation.py
@@ -12,9 +12,9 @@ v_tx = []
 
 v_rx_amp, v_rx_preamp, v_tx, time = plotter.get_voltages_from_file(RECORDED_DATA_FILENAME)
 
-plt.plot(time, v_rx_amp, label="Amplified Rcv V")
-plt.plot(time, v_rx_preamp, label="Pre-amplified Rcv V")
-plt.plot(time, v_tx, label="V Tx from Transmitter")
+plt.plot(time, v_rx_amp, label="Amplified Rcv V", color='cyan')
+plt.plot(time, v_rx_preamp, label="Pre-amplified Rcv V", color='orange')
+plt.plot(time, v_tx, label="V Tx from Transmitter", color='green')
 
 plotter.format_plot('Time [s]', 'Voltage [mV]',
                 'IR Transmission\nReceived 6 messages (32bit each)')
