4186135030fa170221ba1165abb154a6465454e4
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-05-09 11:41:57 +0200

    Remove noise from transmission and save messages in a messages arraylist

--- src/voltage_measurements/plotter.py
+++ src/voltage_measurements/plotter.py
@@ -3,7 +3,6 @@
 import matplotlib.pyplot as plt
 import sys
 
-
 def main():
 
     RECORDED_DATA_FILENAME = "out.csv"
@@ -13,11 +12,15 @@ def main():
     v_rx_preamp = []
     v_tx = []
 
-    v_rx_amp, v_rx_preamp, v_tx, time = get_voltages_from_file(RECORDED_DATA_FILENAME)
+    v_rx_amp, v_rx_preamp, v_tx, time = get_voltages_from_file(
+        RECORDED_DATA_FILENAME)
 
-    plt.plot(time, v_rx_amp, label="Amplified Rcv V")
-    plt.plot(time, v_rx_preamp, label="Pre-amplified Rcv V")
-    plt.plot(time, v_tx, label="V Tx from Transmitter")
+    # Plot first message
+    rx_messages, tx_messages, time_acc = get_IR_messages(v_rx_preamp, v_tx, time)
+
+    plt.plot(time_acc[0], rx_messages[0], label="Messages pre-amp", color='orange')
+
+    plt.plot(time_acc[1], rx_messages[1], label="Messages pre-amp", color='red')
 
     format_plot('Time [s]', 'Voltage [mV]',
                 'IR Transmission\nReceived 6 messages (32bit each)')
@@ -72,6 +75,48 @@ def delete_wrong_records(records):
             continue
 
     return v_rx_amp, v_rx_preamp, v_tx, time
+
+
+def get_IR_messages(v_rx_preamp, v_tx, time):
+    MSG_THRESHOLD = 150
+    MSG_DURATION = 100
+
+    IR_rx_msg = []
+    IR_tx_msg = []
+    msg_time = []
+
+    rx_messages = []
+    tx_messages = []
+    time_acc = []
+
+    i = 0
+    while i < (len(time)):
+
+        if (v_rx_preamp[i] > MSG_THRESHOLD):
+            start_of_msg = i
+            #import pdb; pdb.set_trace()
+
+            while(i < (start_of_msg + MSG_DURATION)):
+                IR_rx_msg.append(v_rx_preamp[i])
+                IR_tx_msg.append(v_tx[i])
+                msg_time.append(time[i])
+                i += 1
+
+            rx_messages.append(IR_rx_msg)
+            tx_messages.append(IR_tx_msg)
+            time_acc.append(msg_time)
+
+            IR_rx_msg = []
+            IR_tx_msg = []
+            msg_time = []
+            
+            continue
+
+        i += 1
+
+    import pdb; pdb.set_trace()
+
+    return rx_messages, tx_messages, time_acc
 
 
 if __name__ == "__main__":
