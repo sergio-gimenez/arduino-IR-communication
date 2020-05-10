fa448f375d7c2fea11fc438fe1ad7b3ddea3f70c
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-05-10 16:36:26 +0200

    Avg amplitud plot is working, just need to do the last adjustments

--- src/voltage_measurements/plotter.py
+++ src/voltage_measurements/plotter.py
@@ -17,20 +17,28 @@ def main():
     v_rx_amp, v_rx_preamp, v_tx, time = get_voltages_from_file(
         RECORDED_DATA_FILENAME)
 
-    rx_messages, time_acc = get_IR_messages(v_rx_preamp, time)
-    tx_messages, time_acce = (get_IR_messages(v_tx, time))
+    rx_messages = get_IR_messages(v_rx_preamp, time)[0]
+    tx_messages = get_IR_messages(v_tx, time)[0]
+    time_acc = get_IR_messages(v_tx, time)[1]
 
-    #plt.plot(time_acc[0], rx_messages[0], label="Messages pre-amp", color='orange')
-    #plt.plot(time_acc[1], rx_messages[1], label="Messages pre-amp", color='red')
-    #format_plot('Time [s]', 'Voltage [mV]', " ")
-  
-    non_zeros_rx = remove_PWM_gaps(rx_messages)
-    
+    non_zeros_rx = remove_PWM_gaps(rx_messages)    
     non_zeros_tx = remove_PWM_gaps(tx_messages)
 
     avg_rx_means = compute_avg(non_zeros_rx)
-    avg_tx_means = compute_avg(non_zeros_tx)  
+    avg_tx_means = compute_avg(non_zeros_tx)
+
+    avg_rx_amplitude = get_avg_voltage_ready_to_plot(avg_rx_means, v_rx_preamp, time)
+    avg_tx_amplitude = get_avg_voltage_ready_to_plot(avg_tx_means, v_tx, time)
+
+    plt.plot(time, v_rx_preamp, label="Pre-amplified Rcv V", color='orange')
+    plt.plot(time, v_tx, label="V Tx from Transmitter", color='green')
+    plt.plot(time, avg_rx_amplitude, label="Avg amplitude", color='red') 
+    plt.plot(time, avg_tx_amplitude, label="Avg amplitude", color='darkgreen') 
     
+    format_plot('Time [s]', 'Voltage [mV]',
+                    'IR Transmission\nReceived 6 messages (32bit each)')
+
+
 
 def get_voltages_from_file(output_filename):
     # Read the file and save it in an array without the \n character
@@ -42,7 +50,7 @@ def format_plot(x_axis_label, y_axis_label, title):
     plt.xlabel(x_axis_label)
     plt.ylabel(y_axis_label)
     plt.title(title)
-    plt.xlim(0, 3500)
+    plt.xlim(0, 2000)
     plt.ylim(0, 5000)
     plt.grid()
     plt.legend()
@@ -92,12 +100,15 @@ def get_IR_messages(IR_transmission, time):
     IR_messages = []
     time_messages = []
 
+    start_of_messages = []
+
     i = 0
     while i < (len(time)):
 
         # Remove noise and only get IR messages
         if (IR_transmission[i] > MSG_NOISE_THRESHOLD):
             start_of_msg = i
+            start_of_messages.append(start_of_msg)
 
             while(i < (start_of_msg + MSG_DURATION)):
                 IR_msg.append(IR_transmission[i])
@@ -115,7 +126,7 @@ def get_IR_messages(IR_transmission, time):
 
         i += 1
     
-    return IR_messages, time_messages
+    return IR_messages, time_messages, start_of_messages
 
 def remove_PWM_gaps(IR_messages):
 
@@ -147,13 +158,59 @@ def compute_avg(no_zeros_msgs):
     return means
     
 
-def plot_means(means, IR_transmission, time):
-    #get_IR_messages(IR_transmission, time)
-    pass
+def plot_mean(means, IR_transmission, time):
+    avg_v = []
+    msg_counter = 0
+    start_of_messages = get_IR_messages(IR_transmission, time)[2]
+
+    i = 0
+    while i < len(time):
+        if (time[i] != start_of_messages[msg_counter]):
+            avg_v.append(0)
+
+        else:
+            avg_v.append(means[msg_counter][0])
+            end_of_msg =  start_of_messages[msg_counter] + MSG_DURATION
+            while i < end_of_msg:
+                avg_v.append(means[msg_counter][0])
+                i += 1
+
+        msg_counter += 1
+
+    plt.plot(time, avg_v, label="AVG Amplitude", color='blue')
 
 
+def get_avg_voltage_ready_to_plot(means, IR_transmission, time):
 
+    start_of_messages = get_IR_messages(IR_transmission, time)[2]
 
+    number_of_messages = len(start_of_messages) - 1 # Since 0 counts
+
+    end_of_messages = []
+    for i in range(len(start_of_messages)):
+        end_of_messages.append(start_of_messages[i] + MSG_DURATION)
+    
+
+    avg_v = []
+    msg_counter = 0
+
+    i = 0
+    while i < len(time):
+        if(i < start_of_messages[msg_counter]):
+            avg_v.append(0)
+        else:
+            
+            avg_v.append(means[msg_counter])
+            while i < end_of_messages[msg_counter]:
+                avg_v.append(means[msg_counter])
+                i+=1
+            
+            if(msg_counter < number_of_messages):
+                msg_counter+=1
+        
+        i+=1
+    
+    return avg_v
 
 if __name__ == "__main__":
     main()
