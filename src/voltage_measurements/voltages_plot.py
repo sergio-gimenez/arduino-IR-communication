e5ebdc5b2014e419126e8f356e9595b88575eeda
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-05-17 09:26:45 +0200

    Added main function in voltages_plot

--- src/voltage_measurements/voltages_plot.py
+++ src/voltage_measurements/voltages_plot.py
@@ -3,18 +3,31 @@
 import plotter
 import matplotlib.pyplot as plt
 
-RECORDED_DATA_FILENAME = "out.csv"
-time = []
-v_rx_amp = []
-v_rx_preamp = []
-v_tx = []
+def main():
+    RECORDED_DATA_FILENAME = "out.csv"
+    time = []
+    v_rx_amp = []
+    v_rx_preamp = []
+    v_tx = []
 
-v_rx_amp, v_rx_preamp, v_tx, time = plotter.get_voltages_from_file(RECORDED_DATA_FILENAME)
+    v_rx_amp, v_rx_preamp, v_tx, time = plotter.get_voltages_from_file(RECORDED_DATA_FILENAME)
 
-plt.plot(time, v_rx_preamp, label="Pre-amplified Rcv V", color='orange')
-plt.plot(time, v_rx_amp, label="Amplified Rcv V", color='cyan')
-plt.plot(time, v_tx, label="V Tx from Transmitter", color='green')
+    plt.plot(time, v_rx_preamp, label="Pre-amplified Rcv V", color='orange')
+    plt.plot(time, v_rx_amp, label="Amplified Rcv V", color='cyan')
+    plt.plot(time, v_tx, label="V Tx from Transmitter", color='green')
 
-plotter.format_plot('Time [ms]', 'Voltage [mV]',
-            'IR Transmission\nReceived 6 messages (32bit each)')
+    format_plot('Time [ms]', 'Voltage [mV]',
+                'IR Transmission\nReceived 6 messages (32bit each)')
 
+def format_plot(x_axis_label, y_axis_label, title):
+    plt.xlabel(x_axis_label)
+    plt.ylabel(y_axis_label)
+    plt.title(title)
+    plt.xlim(0, 3500)
+    plt.ylim(0, 5000)
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
