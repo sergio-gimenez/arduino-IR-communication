24e97ed484220f38f3fc69c12cb866f574afdbb7
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-06-05 21:38:53 +0200

    Able to send random numbers via serial-IR

--- src/serial_ir/receive_serial_ir/receive_serial_ir.ino
+++ src/serial_ir/receive_serial_ir/receive_serial_ir.ino
@@ -1,17 +1,17 @@
 
 void setup() {
- Serial.begin(2400) ;
+  Serial.begin(2400) ;
 }
 
 void loop() {
- int n, i, ch ;
- 
- n = Serial.available() ;
- if (n > 0) {
-       i = n ;
-   while (i--) {
-     ch = Serial.read() ;
-     Serial.print((char)ch) ;
-   }
- }
+  int bytes_to_read_in_buffer, i, current_byte;
+
+  bytes_to_read_in_buffer = Serial.available();
+  if (bytes_to_read_in_buffer > 0) {
+    i = bytes_to_read_in_buffer;
+    while (i--) {
+      current_byte = Serial.read();
+      Serial.print((char) current_byte);
+    }
+  }
 }
